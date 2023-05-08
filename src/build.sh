8c2767c563ac0dc5538b2bf625a8f30635b35602
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-05-08 11:41:30 +0800

    update data space

--- src/build.sh
+++ src/build.sh
@@ -45,7 +45,9 @@ mv /dnscrypt/v3/public-resolvers.m* /src/dnscrypt-resolvers/
 
 # trackerlist
 curl -s https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt >/tmp/trackerslist.txt
+echo "" >>/tmp/trackerslist.txt
 curl -s https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt >>/tmp/trackerslist.txt
+echo "" >>/tmp/trackerslist.txt
 sort -u /tmp/trackerslist.txt >/src/trackerslist.txt
 
 # apk mirrors
