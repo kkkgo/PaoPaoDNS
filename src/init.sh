ed0bd93dab9e66b097731cd1f8b3dedb17522e9b
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-31 01:24:07 +0800

    add CUSTOM_FORWARD addinfo fea

--- src/init.sh
+++ src/init.sh
@@ -256,7 +256,11 @@ if [ "$CNAUTO" != "no" ]; then
     if echo "$CUSTOM_FORWARD" | grep -Eoq ":[0-9]+"; then
         CUSTOM_FORWARD=$(echo "$CUSTOM_FORWARD" | sed 's/"//g')
         sed -i "s/#customforward-seted//g" /tmp/mosdns.yaml
+        CUSTOM_FORWARD_SERVER=$(echo "$CUSTOM_FORWARD" | cut -d":" -f1)
+        CUSTOM_FORWARD_PORT=$(echo "$CUSTOM_FORWARD" | cut -d":" -f2)
         sed -i "s/{CUSTOM_FORWARD}/$CUSTOM_FORWARD/g" /tmp/mosdns.yaml
+        sed -i "s/{CUSTOM_FORWARD_SERVER}/$CUSTOM_FORWARD_SERVER/g" /tmp/mosdns.yaml
+        sed -i "s/{CUSTOM_FORWARD_PORT}/$CUSTOM_FORWARD_PORT/g" /tmp/mosdns.yaml
         if [ ! -f /data/force_forward_list.txt ]; then
             cp /usr/sbin/force_forward_list.txt /data/
         fi
